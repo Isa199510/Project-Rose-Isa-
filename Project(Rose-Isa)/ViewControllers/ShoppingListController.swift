8e48f6a9c0a33ba701764b462dbd9c599b933a5c
Isa <isa.arbuhov@mail.ru>
2023-02-19 11:26:16 +0300

    additing addButton, functions

--- Project(Rose-Isa)/ViewControllers/ShoppingListController.swift
+++ Project(Rose-Isa)/ViewControllers/ShoppingListController.swift
@@ -9,76 +9,125 @@ import UIKit
 
 class ShoppingListController: UITableViewController {
     
-    let items = [1, 2, 3, 4, 5]
-    let shoppings = getShoppingList()
+    var shoppings = getShoppingList()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-
+    
+    @IBAction func newShopping(_ sender: UIBarButtonItem) {
+        let alert = self.alertForShopping("", at: nil) { title in
+            self.newElememtShopping(title)
+        }
+        present(alert, animated: true)
+    }
+    
     // MARK: - Table view data source
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         shoppings.count
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let shoppingCell = tableView.dequeueReusableCell(withIdentifier: "shoppingCell", for: indexPath) as? ShoppingViewCell else { return UITableViewCell()}
-
+        guard let shoppingCell = tableView.dequeueReusableCell(withIdentifier: "shoppingCell", for: indexPath) as? ShoppingViewCell
+        else { return UITableViewCell()}
         shoppingCell.configure(shoppings[indexPath.row])
-
         return shoppingCell
     }
     
-
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         80
     }
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let edit = UIContextualAction(style: .normal, title: "") { (ac: UIContextualAction, view: UIView, succes:(Bool) -> Void) in
+
+            let alert = self.alertForShopping(self.shoppings[indexPath.row].name, at: indexPath.row) {title in
+                self.edittingElement(title, at: indexPath.row)
+            }
+            self.present(alert, animated: true)
+            succes(true)
+        }
+        edit.image = UIImage(systemName: "pencil")
+        edit.backgroundColor = .gray
+        
+        let delete = UIContextualAction(style: .normal, title: "") { (ac: UIContextualAction, view: UIView, succes:(Bool) -> Void) in
+
+            self.deleteElementShopping(at: indexPath.row)
+            succes(true)
+        }
+        delete.image = UIImage(systemName: "trash")
+        delete.backgroundColor = .red
+        
+        let swipeAction = UISwipeActionsConfiguration(actions: [delete, edit])
+        swipeAction.performsFirstActionWithFullSwipe = false
+        
+        return swipeAction
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        
+        
+        let delete = UIContextualAction(style: .normal, title: "") { (ac: UIContextualAction, view: UIView, succes:(Bool) -> Void) in
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            let cell = self.tableView.cellForRow(at: indexPath)
+            cell?.backgroundColor = .green
+            succes(true)
+        }
+        delete.image = UIImage(systemName: delete.image == UIImage(systemName: "checkmark") ? "clear" : "checkmark")
+        delete.backgroundColor = delete.backgroundColor == .green ? .red : .green
+        
+        let swipeAction = UISwipeActionsConfiguration(actions: [delete])
+        return swipeAction
     }
-    */
+}
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+extension ShoppingListController {
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if let groceryVC = segue.destination as? GroceryListController {
+//            groceryVC.shopping = shoppings[sender as? Int ?? 0] as ShoppingLists
+//            groceryVC.at = sender as? Int ?? 0
+//        }
+//    }
+    
+    private func edittingElement(_ title: String, at: Int) {
+        let indexPath = IndexPath(row: at, section: 0)
+        shoppings[at].name = title
+        tableView.reloadRows(at: [indexPath], with: .automatic)
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+    private func newElememtShopping(_ title: String) {
+        let indexPath = IndexPath(row: 0, section: 0)
+        shoppings.insert(ShoppingLists(title), at: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func deleteElementShopping(at: Int) {
+        let indexPath = IndexPath(row: at, section: 0)
+        shoppings.remove(at: at)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
     }
-    */
+ 
+    func alertForShopping(_ text: String, at: Int?, handler: @escaping(_ title: String) -> Void) -> UIAlertController {
 
+        let title = at != nil ? "Изменение записи" : "Новая запись"
+        
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        alert.addTextField()
+        alert.textFields?.first?.text = text
+        let cancel = UIAlertAction(title: "Cancel", style: .destructive)
+        let ok = UIAlertAction(title: "OK", style: .default) { _ in
+            guard let text = alert.textFields?.first?.text else { return }
+            if text.count > 3 {
+                handler(text)
+            }
+        }
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        
+        return alert
+    }
+    
 }
